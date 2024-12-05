bc204bd1268e67e25728dbf4d5581bf9481b5cd5
Manoel V. L. de Oliveira <140553447+Tewdric@users.noreply.github.com>
2024-12-05 17:09:17 -0400

    subindo

--- raposas/script.js
+++ raposas/script.js
@@ -1,9 +1,9 @@
 const valor = document.getElementById("valor")
 const botao = document.getElementById("botao")
 
-botao.addEventListener('click', (e) =>{
-    buscarDados(valor.value)
-})
+// botao.addEventListener('click', (e) =>{
+//     buscarDados(valor.value)
+// })
 
 async function buscarDados(numero) {
     try{
@@ -14,5 +14,63 @@ async function buscarDados(numero) {
     } catch (error){
         console.error('error:', error)
     }
+}
+
+
+
+const lista = [{
+    titulo:'testando',
+    conteudo:'alskdjçasjkashfjhasfkjahfjhaksjfnha'
+},
+{
+    titulo:'testando2',
+    conteudo:'alskdjçasjkashfjhasfkjahfjhaksjfnha22222'
+},
+{
+    titulo:'testando3',
+    conteudo:'alskdjçasjkashfjhasfkjahfjhaksjfnha3333'
+},
+]
+
+var contador = 0
+const btn_soma =document.getElementById('btn_soma')
+const btn_subtracao =document.getElementById('btn_subtracao')
+
+
+
+
+conteudoTexto()
+
+function conteudoTexto(valor){
+    const titulo = document.getElementById('titulo')
+    const conteudo = document.getElementById('conteudo')
+
+    if(valor=='+'){
+        contador++
+        console.log(contador)
+        if(contador==lista.length-1){
+            btn_subtracao.style.visibility='visible'
+            btn_soma.style.visibility='hidden'
+        }
+        else{
+            btn_soma.style.visibility='visible'
+        }
+
+    }
+    else if(valor=='-'){
+        
+        contador--
+        console.log(contador)
+       
+        if(contador==0){
+            btn_subtracao.style.visibility='hidden'
+            btn_soma.style.visibility='visible'
+        }
+
+    }
+
+    titulo.innerHTML= lista[contador].titulo
+    conteudo.innerHTML= lista[contador].conteudo
+
 }
 
