7e10744c4bb6e779270b4ad6be5397ff6eda2389
Vitória Silva <vitoria54099746@aluno.ms.senac.br>
2024-12-06 17:13:08 -0400

    subindo, CHEIO DE BUG

--- raposas/script.js
+++ raposas/script.js
@@ -13,61 +13,61 @@ async function buscarDados(numero) {
     }
 }
 
-const lista = [{
-    titulo:'testando',
-    conteudo:'alskdjçasjkashfjhasfkjahfjhaksjfnha'
-},
-{
-    titulo:'testando2',
-    conteudo:'alskdjçasjkashfjhasfkjahfjhaksjfnha22222'
-},
-{
-    titulo:'testando3',
-    conteudo:'alskdjçasjkashfjhasfkjahfjhaksjfnha3333'
-},
-]
+// const lista = [{
+//     titulo:'testando',
+//     conteudo:'alskdjçasjkashfjhasfkjahfjhaksjfnha'
+// },
+// {
+//     titulo:'testando2',
+//     conteudo:'alskdjçasjkashfjhasfkjahfjhaksjfnha22222'
+// },
+// {
+//     titulo:'testando3',
+//     conteudo:'alskdjçasjkashfjhasfkjahfjhaksjfnha3333'
+// },
+// ]
 
-var contador = 0
-const btn_soma =document.getElementById('btn_soma')
-const btn_subtracao =document.getElementById('btn_subtracao')
-
-
+// var contador = 0
+// const btn_soma =document.getElementById('btn_soma')
+// const btn_subtracao =document.getElementById('btn_subtracao')
 
 
-conteudoTexto()
 
-function conteudoTexto(valor){
-    const titulo = document.getElementById('titulo')
-    const conteudo = document.getElementById('conteudo')
 
-    if(valor=='+'){
-        contador++
-        console.log(contador)
-        if(contador==lista.length-1){
-            btn_subtracao.style.visibility='visible'
-            btn_soma.style.visibility='hidden'
-        }
-        else{
-            btn_soma.style.visibility='visible'
-        }
+// conteudoTexto()
 
-    }
-    else if(valor=='-'){
+// function conteudoTexto(valor){
+//     const titulo = document.getElementById('titulo')
+//     const conteudo = document.getElementById('conteudo')
+
+//     if(valor=='+'){
+//         contador++
+//         console.log(contador)
+//         if(contador==lista.length-1){
+//             btn_subtracao.style.visibility='visible'
+//             btn_soma.style.visibility='hidden'
+//         }
+//         else{
+//             btn_soma.style.visibility='visible'
+//         }
+
+//     }
+//     else if(valor=='-'){
         
-        contador--
-        console.log(contador)
+//         contador--
+//         console.log(contador)
        
-        if(contador==0){
-            btn_subtracao.style.visibility='hidden'
-            btn_soma.style.visibility='visible'
-        }
+//         if(contador==0){
+//             btn_subtracao.style.visibility='hidden'
+//             btn_soma.style.visibility='visible'
+//         }
 
-    }
+//     }
 
-    titulo.innerHTML= lista[contador].titulo
-    conteudo.innerHTML= lista[contador].conteudo
+//     titulo.innerHTML= lista[contador].titulo
+//     conteudo.innerHTML= lista[contador].conteudo
 
-}
+// }
 
 const imgs = document.getElementById("img") /*para pegar o elemento do html*/
 const img = document.querySelectorAll("#img img") /*qurryselectorall pra pelar o elemento dento do id = img*/
@@ -86,3 +86,38 @@ function carrossel(){
 }
 
 setInterval(carrossel,1800);
+
+/*Inicializar Swiper*/ 
+
+const swiper = new Swiper('.slide_wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+//responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        
+        620: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+
+    }
+  });
